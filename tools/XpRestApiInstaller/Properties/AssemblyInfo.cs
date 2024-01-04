a509b8f78485e44d01c7f5cc73e8e5f89959c2b3
Dirk <dirk@subfifty.de>
2024-01-04 21:51:19 +0100

    Version 3.0

--- tools/XpRestApiInstaller/Properties/AssemblyInfo.cs
+++ tools/XpRestApiInstaller/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XpRestApiInstaller")]
+[assembly: AssemblyTitle("MyFramesInstaller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XpRestApiInstaller")]
-[assembly: AssemblyCopyright("Copyright © C4B Com For Business AG")]
+[assembly: AssemblyProduct("MyFramesInstaller")]
+[assembly: AssemblyCopyright("Copyright © subfifty labs")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
